05c41414414073644bbb4f98e19f02e868a49a70
showe93 <showe1364@gmail.com>
2022-12-15 20:59:15 -0500

    progress on web_scraper.py:

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import db_utils
 from UI_Funcs import *
+from web_scraper import scraper
 
 """Group members:
     Justin Buiel
@@ -7,9 +8,19 @@ from UI_Funcs import *
     Sean Howe"""
 
 
+dict = {
+    "Over Ear Headphones" : 0,
+    "USB Microphones" : 1,
+    "1080p Webcams" : 2,
+    "Capture Cards" : 3,
+    "8-channel Audio Mixers" : 4,
+    "Gaming Laptops" : 5
+}
+
+
 def main():
     db_connection, db_cursor = db_utils.set_up_database()
-    # web scraper call
+    scraper(dict)
     #user_Interface()
     db_utils.shut_down_data_base(db_connection)
 
--- web_scraper.py
+++ web_scraper.py
@@ -1,8 +1,3 @@
-# for Sean: when you finish getting all the items for a category
-# make a call like db_utils.insertion_loop(dict, db_connection, db_cursor, table_num)
-# where dict is a dictionary with unimportant keys and a list or tuple with the 5 data points
-# for each product and table_num is 0 for headphones, 1 for microphones and so on.
-# you can do this in here or in main, but you will need both db_connection and db_cursor
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,16 +9,11 @@ HEADERS_FOR_GET_REQ = (
      'Accept-Language': 'en-US, en;q=0.5'}
 )
 
-# Over Ear Headphones - 0
-# USB Microphones - 1
-# 1080p Webcams - 2
-# Capture Cards - 3
-# 8-channel Audio Mixers - 4
-# Gaming Laptops - 5
 
-def scraper():
-    search_url = create_target_URL()
-    search_results = find_search_results(search_url)
+def scraper(category_dict: dict):
+    for item,key in category_dict:
+        search_url = create_target_URL(item)
+        search_results = find_search_results(search_url, key)
 
 def create_target_URL(keywords: str):
     query_terms = keywords.replace(' ', '+')
@@ -31,43 +21,73 @@ def create_target_URL(keywords: str):
     search_url = f'{base_amazon_search_url}{query_terms}'
     return search_url
 
-def find_search_results(search_url: str):
+def find_search_results(search_url: str, key: int, category_dict: dict):
     listing_counter = 0
     listing_limit = 300
     url_results_page_param = 1
     while listing_counter < listing_limit:
-        search_results = gathering_search_results(search_url , url_results_page_param)
+        results_url_param = f'&page={url_results_page_param}'
+        search_page_url = f'{search_url}{results_url_param}'
+        response = requests.get(search_page_url, headers=HEADERS_FOR_GET_REQ)
+        soup_format = BeautifulSoup(response.content, 'html.parser')
+        search_results = soup_format.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
+        extracting_search_results(search_results, listing_counter, listing_limit, url_results_page_param, key)
 
 
-def gathering_search_results(search_url:str, url_results_page_param:int):
-    results_url_param = f'&page={url_results_page_param}'
-    search_page_url = f'{search_url}{results_url_param}'
-    response = requests.get(search_page_url, headers=HEADERS_FOR_GET_REQ)
-    soup_format = BeautifulSoup(response.content, 'html.parser')
-    search_results = soup_format.find_all('div',
-                                          {'class': 's-result-item', 'data-component-type': 's-search-result'})
     return search_results
 
-def extracting_search_results(search_results):
+def extract_product_name(listing_block):
+    product_name = listing_block.h2.text
+    return product_name
 
-# listing_counter = 0
-# listing_limit = 300
-# url_results_page_param = 1
-# while listing_counter < listing_limit:
-#     results_url_param = f'&page={url_results_page_param}'
-#     search_page_url = f'{search_url}{results_url_param}'
-#     response = requests.get(search_page_url, headers=HEADERS_FOR_GET_REQ)
-#     soup_format = BeautifulSoup(response.content, 'html.parser')
-#     search_results = soup_format.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
-#     for listing in search_results:
-#         listing_counter ++
-#         if listing_counter > listing_limit:
-#            break
-#     db_table_row_data = [null, null, null, null, null]
-#     listing_data[0] = extract_product_name()
-#     listing_data[1] = extract_product_rating()
-#     listing_data[2] = extract_num_ratings()
-#     listing_data[3] = extract_product_price()
-#     listing_data[4] = extract_product_URL()
-#     insert_values_into_db_table(db_cursor, 'table_name', tuple(listing_data))
-#     url_results_page_param ++
+def extract_product_rating(listing_block):
+    try:
+        rating_info = listing_block.find('i', {'class': 'a-icon'}).text
+        return rating_info
+    except AttributeError:
+        return None
+
+
+
+def extract_num_ratings(listing_block):
+    try:
+        num_ratings = listing_block.find('span', {'class': 'a-size-base s-underline-text'}).text
+        return num_ratings
+    except AttributeError:
+        return None
+
+
+def extract_product_price(listing_block):
+    try:
+        price_integer = listing_block.find('span', {'class': 'a-price-whole'}).text
+        price_decimal = listing_block.find('span', {'class': 'a-price-fraction'}).text
+        return price_integer + price_decimal
+    except AttributeError:
+        return None
+
+
+def extract_product_URL(listing_block):
+    try:
+        product_url_segment = listing_block.h2.a['href']
+        complete_product_url = 'https://amazon.com' + product_url_segment
+        return complete_product_url
+    except AttributeError:
+        return None
+
+
+def extracting_search_results(search_results: list, listing_counter: int, listing_limit: int, url_results_page_param: int, key: int):
+    for listing in search_results:
+        listing_counter += 1
+        if listing_counter > listing_limit:
+            break
+        db_table_row_data = [None, None, None, None, None]
+        db_table_row_data[0] = extract_product_name(listing)
+        db_table_row_data[1] = extract_product_rating(listing)
+        db_table_row_data[2] = extract_num_ratings(listing)
+        db_table_row_data[3] = extract_product_price(listing)
+        db_table_row_data[4] = extract_product_URL(listing)
+        insertion_loop(tuple(db_table_row_data), db_connection, db_cursor, key)
+        url_results_page_param += 1
+
+
+
